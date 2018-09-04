025a29add411254a2899844a8759babccd0cbec4
PauloLemgruberJeunon <paulopljsousa@gmail.com>
2018-09-03 23:25:58 -0300

    Corrected some errors in the code butit is still not working properly

--- ag_nn.py
+++ ag_nn.py
@@ -4,66 +4,88 @@ np.set_printoptions(precision=2)
 
 TAM_POP = 20
 TAX_MUT = 0.1
-NUM_LAYERS = 2
-LAYERS_SIZES = [2,1]
+LAYERS_SIZES = [2, 2, 1]
 FEATURE_QTY = 1
 MAXX = 1
 G = 1
 
-def math_functions(data, func_name):
-    if func_name == 'square':
-        return [a*a+5 for a in data]
-    if func_name == 'relu':
-        return np.maximum(data, 0)
-    if func_name == 'tanh':
-        return np.tanh(data)
-    if func_name == 'linear':
-        return np.array(data)
-    if func_name == 'sigmoid':
-        data = np.array(data)
-        result = np.empty(data.shape)
-        for i in range(data.shape[0]):
-            result[i] = 1.0 / (1.0 + np.exp(-data[i]))
-        return result
-    return None
 
-def create_neural_net(num_layers, layers_sizes, feature_qty):
-    neural_net = np.zeros(num_layers, dtype=np.ndarray)
+def math_function(x_array):
+    return [x + 5 for x in x_array]
+
+
+def relu(values_array):
+    return np.array([np.maximum(value, 0) for value in values_array], dtype=float)
+
+
+def linear(values_array):
+    return values_array
+
+
+LAYERS_ACVT_FUNC = [linear, linear, linear]
+
+
+# def math_functions(data, func_name):
+#     if func_name == 'square':
+#         return [a * a + 5 for a in data]
+#     if func_name == 'relu':
+#         return np.maximum(data, 0)
+#     if func_name == 'tanh':
+#         return np.tanh(data)
+#     if func_name == 'linear':
+#         return np.array(data)
+#     if func_name == 'sigmoid':
+#         data = np.array(data)
+#         result = np.empty(data.shape)
+#         for i in range(data.shape[0]):
+#             result[i] = 1.0 / (1.0 + np.exp(-data[i]))
+#         return result
+#     return None
+
+
+def create_neural_net(layers_sizes, feature_qty):
+    neural_net = np.zeros(len(layers_sizes), dtype=np.ndarray)
     neural_net[0] = np.random.uniform(-1, 1, (feature_qty + 1, layers_sizes[0]))
 
-    for i in range(1, num_layers):
+    for i in range(1, len(layers_sizes)):
         neural_net[i] = np.random.uniform(-1, 1, (layers_sizes[i - 1] + 1, layers_sizes[i]))
     return neural_net
 
-def predict(neural_net, features, activation_func):
+
+def predict(neural_net, features):
     num_layers = neural_net.shape[0]
     result = np.array(np.append(features, 1))
     for i in range(num_layers):
         result = np.matmul(result, neural_net[i])
-        result = math_functions(result, activation_func)
+        result = LAYERS_ACVT_FUNC[i](result)
         result = np.append(result, 1)
     result = np.delete(result, -1)
     return result
 
+
 def init_pop():
     ind = np.zeros(TAM_POP, dtype=np.ndarray)
     for i in range(TAM_POP):
-        ind[i] = create_neural_net(NUM_LAYERS, LAYERS_SIZES, FEATURE_QTY)
+        ind[i] = create_neural_net(LAYERS_SIZES, FEATURE_QTY)
     return ind
 
-def avaliation(ind, features, y_data):
+
+def avaliation(ind, x_data, y_data):
     fit = np.zeros(TAM_POP)
     # Temp array used to recive the predicted values from the neural network.
-    predict_temp = np.zeros(len(features))
+    predict_temp = np.zeros(len(x_data))
     for i in range(TAM_POP):  # For each neural network.
-        for j in range(len(features)):  # For each value to be predicted.
-            predict_temp[j] = predict(ind[i], features[j], 'relu')
+        for j in range(len(x_data)):  # For each value to be predicted.
+            predict_temp[j] = predict(ind[i], x_data[j])
         # Calculates fit using the mean square error function of the predicted values and the correct values.
-        fit[i] = 1+1/mqe(predict_temp, y_data)
+        a = 1 / (0.001 + mqe(predict_temp, y_data))
+        fit[i] = a
 
     print('Correct Data:\n', *y_data, '\n\n')
     print('Data from Neural Network:\n', predict_temp)
+    print('fit = ', fit)
     return fit
+
 
 def crossover(ind, fit):
     maxfit = fit[0]
@@ -77,19 +99,21 @@ def crossover(ind, fit):
         if i == maxi:
             continue  # Protect the best.
         # Crossover
-        ind[i] = (ind[i] + ind[maxi])/2  # Using arithmetic mean.
+        ind[i] = (ind[i] + ind[maxi]) / 2  # Using arithmetic mean.
     # Mutation
-    ind[i] = ind[i] + ((np.random.uniform(-1, 1) % MAXX-(MAXX/2.0))/100.0)*TAX_MUT  # Very important!
+    ind[i] = ind[i] + ((np.random.uniform(-1, 1) % MAXX - (MAXX / 2.0)) /
+                       100.0) * TAX_MUT  # Very important!
+
 
 DATA_RANGE = (-50, 51, 1)
-FEATURES = np.arange(*DATA_RANGE)
-Y_DATA = np.array(math_functions(FEATURES, 'square'))
+X_DATA = np.arange(*DATA_RANGE)
+Y_DATA = math_function(X_DATA)
 
 IND = init_pop()  # Start population with random values in a range.
 
-while 1:
+while G < 201000:
     print('\n')
     print('AG Generation:', G, '\n')
-    FIT = avaliation(IND, FEATURES, Y_DATA)
+    FIT = avaliation(IND, X_DATA, Y_DATA)
     crossover(IND, FIT)
-    G = G+1
+    G = G + 1
