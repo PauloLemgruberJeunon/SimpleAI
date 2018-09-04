112f113a1cfadf9f82de2c7ce1416c7f01fb93d9
PauloLemgruberJeunon <paulopljsousa@gmail.com>
2018-09-04 01:37:22 -0300

    Changed the mutation and crossover algs

--- ag_nn.py
+++ ag_nn.py
@@ -1,9 +1,10 @@
 import numpy as np
+import heapq
 from sklearn.metrics import mean_squared_error as mqe
 np.set_printoptions(precision=2)
 
 TAM_POP = 20
-TAX_MUT = 0.1
+TAX_MUT = 0.05
 LAYERS_SIZES = [2, 2, 1]
 FEATURE_QTY = 1
 MAXX = 1
@@ -11,7 +12,7 @@ G = 1
 
 
 def math_function(x_array):
-    return [x + 5 for x in x_array]
+    return [(x ** 2) + 5 for x in x_array]
 
 
 def relu(values_array):
@@ -22,30 +23,12 @@ def linear(values_array):
     return values_array
 
 
-LAYERS_ACVT_FUNC = [linear, linear, linear]
-
-
-# def math_functions(data, func_name):
-#     if func_name == 'square':
-#         return [a * a + 5 for a in data]
-#     if func_name == 'relu':
-#         return np.maximum(data, 0)
-#     if func_name == 'tanh':
-#         return np.tanh(data)
-#     if func_name == 'linear':
-#         return np.array(data)
-#     if func_name == 'sigmoid':
-#         data = np.array(data)
-#         result = np.empty(data.shape)
-#         for i in range(data.shape[0]):
-#             result[i] = 1.0 / (1.0 + np.exp(-data[i]))
-#         return result
-#     return None
+LAYERS_ACVT_FUNC = [relu, relu, linear]
 
 
 def create_neural_net(layers_sizes, feature_qty):
     neural_net = np.zeros(len(layers_sizes), dtype=np.ndarray)
-    neural_net[0] = np.random.uniform(-1, 1, (feature_qty + 1, layers_sizes[0]))
+    neural_net[0] = np.random.uniform(-100, 100, (feature_qty + 1, layers_sizes[0]))
 
     for i in range(1, len(layers_sizes)):
         neural_net[i] = np.random.uniform(-1, 1, (layers_sizes[i - 1] + 1, layers_sizes[i]))
@@ -74,35 +57,51 @@ def avaliation(ind, x_data, y_data):
     fit = np.zeros(TAM_POP)
     # Temp array used to recive the predicted values from the neural network.
     predict_temp = np.zeros(len(x_data))
+    min_mqe = 1000000
     for i in range(TAM_POP):  # For each neural network.
         for j in range(len(x_data)):  # For each value to be predicted.
             predict_temp[j] = predict(ind[i], x_data[j])
         # Calculates fit using the mean square error function of the predicted values and the correct values.
-        a = 1 / (0.001 + mqe(predict_temp, y_data))
+        b = mqe(predict_temp, y_data)
+        if b < min_mqe:
+            min_mqe = b
+        a = 1 / (0.001 + b)
         fit[i] = a
 
-    print('Correct Data:\n', *y_data, '\n\n')
-    print('Data from Neural Network:\n', predict_temp)
-    print('fit = ', fit)
+    # print('Correct Data:\n', *y_data, '\n\n')
+    # print('Data from Neural Network:\n', predict_temp)
+    # print('fit = ', fit)
+    print(min_mqe)
     return fit
 
 
 def crossover(ind, fit):
-    maxfit = fit[0]
-    maxi = 0
-    for i in range(1, TAM_POP):  # Search for the best. We don't kill the best!
-        if fit[i] > maxfit:
-            maxfit = fit[i]
-            maxi = i
 
-    for i in range(0, TAM_POP):
+    n_largest_fit_inds = heapq.nlargest(len(fit), range(len(fit)), fit.take)
+
+    maxfit = fit[n_largest_fit_inds[0]]
+    maxi = n_largest_fit_inds[0]
+    delete_point = len(n_largest_fit_inds) // (2 / 3)
+
+    for i in range(1, len(n_largest_fit_inds)):
         if i == maxi:
-            continue  # Protect the best.
-        # Crossover
-        ind[i] = (ind[i] + ind[maxi]) / 2  # Using arithmetic mean.
-    # Mutation
-    ind[i] = ind[i] + ((np.random.uniform(-1, 1) % MAXX - (MAXX / 2.0)) /
-                       100.0) * TAX_MUT  # Very important!
+            continue
+        curr_index = n_largest_fit_inds[i]
+
+        if i >= delete_point:
+            ind[curr_index] = create_neural_net(LAYERS_SIZES, FEATURE_QTY)
+        else:
+            # Using arithmetic mean.
+            ind[curr_index] = (ind[curr_index] + ind[maxi]) / 2
+
+        for k in range(len(ind[curr_index])):
+            curr_matrix = ind[curr_index][k]
+            i_shape, j_shape = ind[curr_index][k].shape
+            for i_coord in range(i_shape):
+                for j_coord in range(j_shape):
+                    if np.random.random() < TAX_MUT:
+                        curr_matrix[i_coord][j_coord] = ind[maxi][k][i_coord][j_coord] * \
+                            2 * (np.random.random() + 0.1)
 
 
 DATA_RANGE = (-50, 51, 1)
