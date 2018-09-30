6174402817a4b863872cbdfdf5df6b43d820052d
Unknown <nali.matheus@gmail.com>
2018-09-30 04:41:25 -0300

    #NuncaMaisEuVouDormir
    kkkk me empolguei, é divertido ficar mexendo nisso..

--- ag_nn.py
+++ ag_nn.py
@@ -1,14 +1,17 @@
 import heapq
 import csv
 from random import randint as rint
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.metrics import mean_squared_error as mqe
+#from sklearn.metrics import mean_squared_error as mqe
 np.set_printoptions(precision=3)
 
+#Investigar 'Common Mistakes' https://stackoverflow.com/questions/41488279/neural-network-always-predicts-the-same-class
+#https://blog.slavv.com/37-reasons-why-your-neural-network-is-not-working-4020854bd607
 
-TAM_POP = 50
+TAM_POP = 30
 TAX_MUT = 0.01
-LAYERS_SIZES = [8, 8, 3]
+LAYERS_SIZES = [15, 15, 3]
 FEATURE_QTY = 4
 MAXX = 1
 G = 1
@@ -56,53 +59,11 @@ def predict(neural_net, features):
     return result
 
 
-def iris_create_neural_net(layers_sizes, feature_qty):
-    neural_net = np.zeros(len(layers_sizes), dtype=np.ndarray)
-    neural_net[0] = np.random.uniform(-1, 1, (feature_qty, layers_sizes[0]))
-
-    for i in range(1, len(layers_sizes)):
-        neural_net[i] = np.random.uniform(-1, 1, (layers_sizes[i - 1], layers_sizes[i]))
-    return neural_net
-
-
-def iris_predict(neural_net, features):
-    num_layers = neural_net.shape[0]
-    result = np.array(features)
-    for i in range(num_layers):
-        result = np.matmul(result, neural_net[i])
-        bias = np.random.uniform(-1, 1, result.shape)
-        result = result + bias
-        result = LAYERS_ACVT_FUNC[i](result)
-    return result
-
-
 def init_pop():
     ind = np.zeros(TAM_POP, dtype=np.ndarray)
     for i in range(TAM_POP):
-        ind[i] = iris_create_neural_net(LAYERS_SIZES, FEATURE_QTY)
+        ind[i] = create_neural_net(LAYERS_SIZES, FEATURE_QTY)
     return ind
-
-
-def avaliation(ind, x_data, y_data):
-    fit = np.zeros(TAM_POP)
-    # Temp array used to recive the predicted values from the neural network.
-    predict_temp = np.zeros(len(x_data))
-    min_mqe = 1000000
-    for i in range(TAM_POP):  # For each neural network.
-        for j in range(len(x_data)):  # For each value to be predicted.
-            predict_temp[j] = predict(ind[i], x_data[j])
-        # Calculates fit using the mean square error function of the predicted values and the correct values.
-        b = mqe(predict_temp, y_data)
-        if b < min_mqe:
-            min_mqe = b
-        a = 1 / (0.001 + b)
-        fit[i] = a
-
-    # print('Correct Data:\n', *y_data, '\n\n')
-    # print('Data from Neural Network:\n', predict_temp)
-    # print('fit = ', fit)
-    print(min_mqe)
-    return fit
 
 
 def iris_avaliation(ind, x_data):
@@ -111,22 +72,22 @@ def iris_avaliation(ind, x_data):
     predict_temp = np.zeros(len(x_data), dtype=np.ndarray)
     for i in range(TAM_POP):  # For each neural network.
         for j in range(len(x_data)):  # For each value to be predicted.
-            predict_temp[j] = iris_predict(ind[i], x_data[j][:4])
+            predict_temp[j] = predict(ind[i], x_data[j][:4])
             
+            #Contabilizando os erros.
             if(x_data[j][4] == 0 and (predict_temp[j][0] < 0.5 or predict_temp[j][1] > 0.5 or predict_temp[j][2] > 0.5)):
-                fit[i] += 1
+                fit[i] += 1 
             if(x_data[j][4] == 1 and (predict_temp[j][1] < 0.5 or predict_temp[j][0] > 0.5 or predict_temp[j][2] > 0.5)):
                 fit[i] += 1
             if(x_data[j][4] == 2 and (predict_temp[j][2] < 0.5 or predict_temp[j][1] > 0.5 or predict_temp[j][0] > 0.5)):
                 fit[i] += 1
 
-            #if G==20:
-                #print('Predict: ',predict_temp[j])
-                #print('Dado correto: ',x_data[j][4])
+            #print('Predict: ', predict_temp[j])
+            #print('Dado correto: ', x_data[j][4])
 
         print('Geração: ', G)
         print('Erros: ', fit[i])
-        fit[i] = 1/((fit[i]/len(x_data))+1) 
+        fit[i] = 1/((fit[i]/len(x_data))+1)  #https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=4667170
         print('Fit: ', fit[i])
 
     return fit
@@ -171,27 +132,30 @@ def iris_crossover(ind, fit):
         if i == maxi:
             continue
 
-        a = rint(0,TAM_POP-1)
-        b = rint(0,TAM_POP-1)
+        a = rint(0, TAM_POP-1)
+        b = rint(0, TAM_POP-1)
         if fit[a] > fit[b]:
             pai1 = a
         else:
             pai1 = b
 
-        a = rint(0,TAM_POP-1)
-        b = rint(0,TAM_POP-1)
+        a = rint(0, TAM_POP-1)
+        b = rint(0, TAM_POP-1)
         if fit[a] > fit[b]:
             pai2 = a
         else:
             pai2 = b
 
-        CROSSOVER_INDEX = [pai1, pai2, maxi]
-
+        crossover_index = [pai1, pai2, maxi]
+        k = 0
         for j in range(len(LAYERS_SIZES)):
             rand = rint(0, len(LAYERS_SIZES)-1)
-            ind[i][rand] = ind[CROSSOVER_INDEX[j]][rand]
+            ind[i][rand] = ind[crossover_index[k]][rand]
             rand = rint(0, len(LAYERS_SIZES)-1)
-            ind[i][rand] = ind[i][rand] + np.random.uniform(-1,1)*TAX_MUT
+            ind[i][rand] = ind[i][rand] + np.random.uniform(-1, 1) * TAX_MUT
+            k += 1
+            if k == 3:
+                k = 0
 
 IND = init_pop()  # Start population with random values in a range.
 
@@ -202,6 +166,34 @@ with open("IrisDataSet/iris.data") as csv_file:
         DATA.append(row)
     DATA.pop()
 DATA = np.array(DATA, dtype=np.float64)
+
+plt.plot(DATA[0:50, 0:1], 'o')
+plt.plot(DATA[50:100, 0:1], 'o')
+plt.plot(DATA[100:150, 0:1], 'o')
+plt.xlabel('sepal length in cm')
+plt.ylabel('sepal width in cm')
+plt.legend('012')
+#plt.show()
+
+plt.plot(DATA[0:50, 2:3], 'o')
+plt.plot(DATA[50:100, 2:3], 'o')
+plt.plot(DATA[100:150, 2:3], 'o')
+plt.xlabel('petal length in cm')
+plt.ylabel('petal width in cm')
+plt.legend('012')
+#plt.show()
+
+# Média zero
+DATA[:,0] = DATA[:,0] - np.mean(DATA[:,0])
+DATA[:,1] = DATA[:,1] - np.mean(DATA[:,1])
+DATA[:,2] = DATA[:,2] - np.mean(DATA[:,2])
+DATA[:,3] = DATA[:,3] - np.mean(DATA[:,3])
+
+# Variância 1
+DATA[:,0] = DATA[:,0]/np.std(DATA[:,0])
+DATA[:,1] = DATA[:,1]/np.std(DATA[:,1])
+DATA[:,2] = DATA[:,2]/np.std(DATA[:,2])
+DATA[:,3] = DATA[:,3]/np.std(DATA[:,3])
 
 while G < 201:
     print('\n')
